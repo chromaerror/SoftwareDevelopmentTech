c83b97363c0ca10535e9c6608c381e7dc5a34344
Jaana <jaana@proto366.haagahelia.amk>
2019-02-07 22:35:31 +0200

    Added database integration

--- list.php
+++ list.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<title>Login successful</title>
-</head>
-
-<body>
-	<h1>Login successful</h1>
-</body>
-
-</html>
+<?php
+	session_start();
+	if($_SESSION['username'] == 'username') {
+		include "connect.php";
+		print("<html>");
+		$conn = connect_db();
+		$sql = "SELECT * FROM Addresses";
+		$result = mysqli_query($conn, $sql);
+		if (mysqli_num_rows($result) > 0) {
+			// output data of each row
+			while($row = mysqli_fetch_assoc($result)) {
+				echo $row["id"] . " ";
+				echo $row["name"] . " ";
+				echo $row["street_address"] . " ";
+				echo $row["zip"] . " ";
+				echo $row["city"] . " ";
+				echo $row["state"] . " ";
+				echo $row["country"] . " ";
+				print("<br>");
+			}
+		} else {
+			echo "0 results";
+		}
+		print("</html>");
+		mysqli_close($conn);
+	} else {
+ 		print("No access.");
+	}
+?>
